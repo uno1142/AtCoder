c3a3c0aff7b1422a6e58925bf5f3d1c3056c62fd
YusukeTakayama <64245094+uno1142@users.noreply.github.com>
2020-05-16 09:16:09 +0900

    Create 足したり引いたり.cpp

---
+++ trainingAPG4b/足したり引いたり.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string S;
+  cin >> S;
+  int x = S.size();
+  
+  int count_p = 0;
+  int count_m = 0;
+  for(int i=0; i <x; i++){
+    if(S.at(i) == '+'){
+      count_p++;
+    }
+    else if(S.at(i) == '-'){
+      count_m++;
+    }
+  }
+  cout << count_p - count_m + 1<< endl;
+}
+
+
+//解答例はint answer = 1で書いてif文で++と--をanswerに使ってた。これだとACは出るが無駄が多い
