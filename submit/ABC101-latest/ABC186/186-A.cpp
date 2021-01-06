61be80910d2eb07452bfddd36a87add35045768b
uno1142 <byeethe8@yahoo.co.jp>
2021-01-06 22:56:12 +0900

    add

---
+++ submit/ABC101-latest/ABC186/186-A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for (int i =0; i <(n); i++)
+using namespace std;
+using ll = long long;
+const double PI = 3.14159265358979323846;
+const ll MOD = 1000000007;
+
+int main(){
+    int n,w;
+    cin >> n >> w;
+    cout << n / w << endl;
+    return 0;
+
+
+}
