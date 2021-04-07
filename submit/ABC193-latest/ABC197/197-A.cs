6e26690c66abfe795ab877ed90c03c6d4cc09a74
uno1142 <byeethe8@yahoo.co.jp>
2021-04-07 21:06:06 +0900

    rename

---
+++ submit/ABC193-latest/ABC197/197-A.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string S = Console.ReadLine();
+			
+		}
+	}
+	
+}
